9b41e82f93480511c657a0ad3d636122c18071f8
Bintang <starsyndrome0912@gmail.com>
2024-03-19 00:01:37 +0700

    Remove space

--- src/main/java/org/metrodataacademy/TugasSpringBoot/TugasSpringBootApplication.java
+++ src/main/java/org/metrodataacademy/TugasSpringBoot/TugasSpringBootApplication.java
@@ -11,7 +11,6 @@ public class TugasSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TugasSpringBootApplication.class, args);
-		
 		log.info("First Project Spring Boot!");
  	}
 }
