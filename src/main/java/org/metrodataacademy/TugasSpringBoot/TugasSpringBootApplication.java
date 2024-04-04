9404790fbfb9a3afadb7ea03830e47e2a873be11
Bintang <starsyndrome0912@gmail.com>
2024-04-04 19:02:11 +0700

    Remove @Slf4j from main method

--- src/main/java/org/metrodataacademy/TugasSpringBoot/TugasSpringBootApplication.java
+++ src/main/java/org/metrodataacademy/TugasSpringBoot/TugasSpringBootApplication.java
@@ -3,10 +3,8 @@ package org.metrodataacademy.TugasSpringBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
-@Slf4j
 public class TugasSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TugasSpringBootApplication.class, args);
